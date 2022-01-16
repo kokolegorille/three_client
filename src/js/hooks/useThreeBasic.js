f4fa1dcc6ec135818a0d6bb39b4b08aa29152929
kokolegorille <koko.le.gorille@gmail.com>
2022-01-16 13:15:55 +0100

    Start three class components

---
+++ src/js/hooks/useThreeBasic.js
@@ -0,0 +1,169 @@
+import { useReducer } from "react"
+import * as THREE from "three"
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
+
+import Plane from "../components/plane"
+
+const defaultState = {
+    scene: null,
+    renderer: null,
+    camera: null,
+    localPlayer: null
+}
+
+const START = "START"
+const STOP = "STOP"
+const SET_LOCAL_PLAYER = "SET_LOCAL_PLAYER"
+
+const reducer = (state, action) => {
+    let newState
+    switch (action.type) {
+        case START:
+            newState = {
+                ...state,
+                ...action.payload
+            }
+            break
+        case STOP:
+            newState = defaultState
+            break
+        case SET_LOCAL_PLAYER:
+            newState = {
+                ...state,
+                localPlayer: action.payload
+            }
+            break
+        default: newState = state
+    }
+    console.log(state, action, newState)
+    return newState
+}
+
+// TODO: Wrap this in a custom hook, to avoid having to define this outside...
+let state, dispatch
+
+const useThreeBasic = (initialState = defaultState) => {
+    [state, dispatch] = useReducer(reducer, initialState)
+
+    const start = (canvas, width, height) => {
+        // Camera
+        const camera = new THREE.PerspectiveCamera( 70, width / height, 0.01, 100 )
+        camera.position.set( -4.37, 0, -4.75 )
+        camera.lookAt(0, 0, 6)
+
+        // Camera controller
+        const cameraController = new THREE.Object3D()
+        cameraController.add(camera)
+
+        // Lights
+        const ambient = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 0.5)
+        const light = new THREE.DirectionalLight(0xffffff, 1.5)
+
+        // Scene
+        const scene = new THREE.Scene()
+        scene.add(cameraController)
+        scene.add(ambient)
+        scene.add(light)
+
+        // Load skybox
+        scene.background = new THREE.CubeTextureLoader()
+            .setPath("../../../assets/plane/paintedsky/")
+            .load( [
+                'px.jpg',
+                'nx.jpg',
+                'py.jpg',
+                'ny.jpg',
+                'pz.jpg',
+                'nz.jpg'
+            ], () => {
+                // this.renderer.setAnimationLoop(this.render.bind(this));
+                console.log("SKY LOADED")
+            } )
+
+        // Renderer
+        const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true })
+        renderer.setPixelRatio( window.devicePixelRatio )
+        renderer.setSize( width, height )
+        renderer.outputEncoding = THREE.sRGBEncoding
+        renderer.physicallyCorrectLights = true
+
+        renderer.setAnimationLoop( animation )
+
+        // Add to canvas
+        canvas.appendChild( renderer.domElement )
+
+        const payload = { camera, scene, renderer }
+        dispatch({type: START, payload})
+    }
+
+    const stop = () => {
+        const { renderer } = state
+        renderer.setAnimationLoop( null )
+        dispatch({type: STOP})
+    }
+
+    // const loadLocalPlayer = (file) => {
+    //     const loader = new GLTFLoader().setPath("../../../assets/")
+    //     loader.load(
+    //         file,
+    //         gltf => {
+    //             const { scene } = state
+    //             scene.add(gltf.scene)
+    //             dispatch({type: SET_LOCAL_PLAYER, payload: gltf})
+    //         },
+    //         xhr => {
+    //             const progress = xhr.loaded / xhr.total
+    //             console.log(xhr.loaded, xhr.total)
+    //             console.log(progress)
+    //         },
+    //         err => {
+    //             console.log(err)
+    //         }
+    //     )
+    // }
+
+    const loadPlane = () => {
+        const callback = gltf => {
+            const { scene } = state
+            scene.add(gltf.scene)
+            dispatch({type: SET_LOCAL_PLAYER, payload: gltf})
+        }
+        new Plane(callback)
+    }
+
+    const resize = (width, height) => {
+        const { renderer, camera } = state
+        if (renderer) {
+            camera.aspect = width / height
+            camera.updateProjectionMatrix()
+            renderer.setSize( width, height )
+        }
+    }
+
+    // Private
+    const animation = time => {
+        const { scene, renderer, camera, localPlayer } = state
+        
+        console.log(time)
+
+        // Update local player
+        if (localPlayer) {
+            // const propeller = localPlayer.scene.getObjectByName("propeller")
+            // if (propeller) { propeller.rotateZ(1) }
+            localPlayer.update(time)
+        }
+
+        renderer && renderer.render(scene, camera)
+    }
+
+    const actions = {
+        start, stop, 
+        // loadLocalPlayer, 
+        loadPlane,
+        resize
+    }
+
+    return {state, actions}
+}
+
+export default useThreeBasic
--- src/js/views/Basic.js
+++ src/js/views/Basic.js
@@ -1,104 +1,33 @@
-import React, { useEffect, useRef } from "react"
-import * as THREE from "three"
-import OrbitControls from "three-orbitcontrols"
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import React, { useRef, useEffect } from "react"
 
 import useWindowSize from "../hooks/useWindowSize"
+import useThreeBasic from "../hooks/useThreeBasic"
 
 const Basic = () => {
     const canvas = useRef()
-    const {width, height} = useWindowSize()
+    const { width, height } = useWindowSize()
+    const { state, actions } = useThreeBasic()
 
-    console.log(width, height)
-
-    let camera, scene, renderer
-    let geometry, material, mesh
-
-    const margin = 10
-
-    const loadGLTF = (file) => {
-        const loader = new GLTFLoader().setPath("../../../assets/plane/")
-        loader.load(
-            file,
-            gltf => {
-                console.log(gltf)
-            },
-            xhr => {
-
-            },
-            err => {
-
-            }
-        )
-    }
+    console.log(state)
 
     useEffect(() => {
-        console.log("Mounted")
+        // Start the game
+        actions.start(canvas.current, width, height)
+        // actions.loadLocalPlayer("plane/microplane.glb")
 
-        // Create the camera
-        camera = new THREE.PerspectiveCamera( 70, width / height, 0.01, 10 )
-        camera.position.z = 1
+        actions.loadPlane()
 
-        // Create the scene
-        scene = new THREE.Scene()
-        scene.background = new THREE.Color(0xaaaaaa)
-
-        // Lights
-        const ambient = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 0.5)
-        scene.add(ambient)
-
-        const light = new THREE.DirectionalLight(0xffffff, 1.5)
-        light.position.set(0.2, 1, 1)
-        scene.add(light)
-
-        // Load GLTF
-        loadGLTF("microplane.glb")
-        
-        // Create a mesh
-        geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 )
-        material = new THREE.MeshNormalMaterial()
-        mesh = new THREE.Mesh( geometry, material )
-
-        // Add the mesh to the scene
-        scene.add( mesh )
-
-        // Create the renderer
-        renderer = new THREE.WebGLRenderer( { antialias: true } )
-        renderer.setSize( width - margin, height - margin )
-        renderer.setAnimationLoop( animation )
-
-        // Controls
-        const controls = new OrbitControls(camera, renderer.domElement)
-        controls.enableDamping = true
-        controls.dampingFactor = 0.25
-        controls.enableToom = false
-
-        // render into the #canvas element
-        canvas.current.appendChild( renderer.domElement )
-
-        return () => {
-            console.log("Unmounted")
-        }
+        // Cleanup
+        return () => { actions.stop() }
     }, [])
 
-    // useEffect(() => {
-    //     console.log(renderer)
+    useEffect(() => {
+        // Resize the scene when dimensions change
+        actions.resize(width, height)
 
-    //     // Renderer is undefined here!
-    //     if (renderer) {
-    //         camera.aspect = width / height
-    //         camera.updateProjectionMatrix()
-    //         renderer.setSize( width - margin, height - margin )
-    //     }
-    // }, [width, height])
-
-    // Animation loop
-    const animation = (time) => {
-        mesh.rotation.x = time / 2000
-	    mesh.rotation.y = time / 1000
-
-	    renderer.render( scene, camera )
-    }
+        // Cleanup
+        // return () => {}
+    }, [width, height])
 
     return (
         <div id='canvas' ref={canvas} />
@@ -106,3 +35,132 @@ const Basic = () => {
 }
 
 export default Basic
+
+// import React, { useEffect, useRef } from "react"
+// import * as THREE from "three"
+// // import OrbitControls from "three-orbitcontrols"
+// import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
+
+// import useWindowSize from "../hooks/useWindowSize"
+
+// const Basic = () => {
+//     const canvas = useRef()
+//     const {width, height} = useWindowSize()
+
+//     console.log(width, height)
+
+//     let camera, scene, renderer
+//     let geometry, material, mesh
+//     let plane, cameraController
+
+//     const margin = 5
+
+//     const loadGLTF = (file, scene) => {
+//         const loader = new GLTFLoader().setPath("../../../assets/")
+//         loader.load(
+//             file,
+//             gltf => {
+//                 console.log(gltf)
+//                 scene.add(gltf.scene)
+//                 plane = gltf
+//             },
+//             xhr => {
+//                 const progress = xhr.loaded / xhr.total
+//                 console.log(xhr.loaded, xhr.total)
+//                 console.log(progress)
+//             },
+//             err => {
+//                 console.log(err)
+//             }
+//         )
+//     }
+
+//     useEffect(() => {
+//         console.log("Mounted")
+
+//         // Create the camera
+//         camera = new THREE.PerspectiveCamera( 70, width / height, 0.01, 100 )
+//         // camera.position.z = 1
+//         // camera.position.set( 0, 0, 5 );
+//         camera.position.set( -4.37, 0, -4.75 )
+//         camera.lookAt(0, 0, 6)
+
+//         cameraController = new THREE.Object3D()
+//         cameraController.add(camera)
+
+//         // Create the scene
+//         scene = new THREE.Scene()
+//         scene.background = new THREE.Color(0xaaaaaa)
+//         scene.add(cameraController)
+
+//         // Lights
+//         const ambient = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 0.5)
+//         scene.add(ambient)
+
+//         const light = new THREE.DirectionalLight(0xffffff, 1.5)
+//         light.position.set(0.2, 1, 1)
+//         scene.add(light)
+
+//         // Load GLTF
+//         loadGLTF("plane/microplane.glb", scene)
+
+//         // Create a mesh
+//         geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 )
+//         material = new THREE.MeshNormalMaterial()
+//         mesh = new THREE.Mesh( geometry, material )
+
+//         // Add the mesh to the scene
+//         scene.add( mesh )
+
+//         // Create the renderer
+//         renderer = new THREE.WebGLRenderer( { antialias: true, alpha: true } )
+//         renderer.setPixelRatio( window.devicePixelRatio )
+//         renderer.setSize( width - margin, height - margin )
+//         renderer.outputEncoding = THREE.sRGBEncoding
+//         renderer.physicallyCorrectLights = true
+//         renderer.setAnimationLoop( animation )
+
+//         // // Controls
+//         // const controls = new OrbitControls(camera, renderer.domElement)
+//         // controls.enableDamping = true
+//         // controls.dampingFactor = 0.25
+//         // controls.enableToom = false
+
+//         // render into the #canvas element
+//         canvas.current.appendChild( renderer.domElement )
+
+//         return () => {
+//             console.log("Unmounted")
+//         }
+//     }, [])
+
+//     useEffect(() => {
+//         console.log("Plane has updated")
+//         console.log(plane)
+//     }, [plane])
+
+//     // useEffect(() => {
+//     //     console.log(renderer)
+
+//     //     // Renderer is undefined here!
+//     //     if (renderer) {
+//     //         camera.aspect = width / height
+//     //         camera.updateProjectionMatrix()
+//     //         renderer.setSize( width - margin, height - margin )
+//     //     }
+//     // }, [width, height])
+
+//     // Animation loop
+//     const animation = (time) => {
+//         mesh.rotation.x = time / 2000
+// 	    mesh.rotation.y = time / 1000
+
+// 	    renderer.render( scene, camera )
+//     }
+
+//     return (
+//         <div id='canvas' ref={canvas} />
+//     )
+// }
+
+// export default Basic
